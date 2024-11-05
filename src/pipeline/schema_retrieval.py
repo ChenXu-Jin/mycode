8d584650844be530d0884871ed2b34d08724f732
ChenXu-Jin <1191333290@qq.com>
2024-11-05 14:21:16 +0000

    2024.11.5 22.21

--- src/pipeline/schema_retrieval.py
+++ src/pipeline/schema_retrieval.py
@@ -1,5 +1,32 @@
-
+import logging
+from llm.llm_models import async_llm_chain_call
+from pipeline.utils import node_decorator
+from pipeline.pipeline_manager import PipelineManager
 from typing import Dict, List, Any
 
+@node_decorator()
 def schema_retrieval() -> Dict[str, List[str]]:
     pass
+
+def get_keywords_from_question(task: Any) -> Dict[str, Any]:
+    request_kwargs = {
+        "HINT": task.evidence,
+        "QUESTION": task.question
+    }
+
+    logging.info("Fetching engine, prompt and parser")
+    engine, prompt, parser = PipelineManager.get_engine_prompt_parser()
+    logging.info("Initiating asynchronous LLM chain call for keyword extraction")
+    response = async_llm_chain_call( 
+        engine=engine,
+        prompt=prompt,
+        parser=parser,
+        request_list=[request_kwargs],
+        step="keyword_extraction",
+        sampling_count=1
+    )[0]
+    keywords = response[0]
+    result = {"keywords": keywords}
+    
+    logging.info(f"Keywords extracted: {keywords}")
+    return result
